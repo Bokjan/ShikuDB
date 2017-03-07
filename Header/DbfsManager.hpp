de626332a06b79948d4678b0eb1abad91126238a
Boyin Chen <i@bokjan.com>
2017-03-08 00:57:16 +0800

    Data structures

--- Header/DbfsManager.hpp
+++ Header/DbfsManager.hpp
@@ -10,16 +10,38 @@
 #include <fcntl.h>
 #include <cstdio>
 #include <cstring>
+#include <cstdint>
 #include <stdexcept>
 namespace shiku
 {
+	struct DiskLoc // sizeof(DiskLoc) == 8
+	{
+		using byte = char;
+		uint32_t file;
+		uint32_t offset;
+	};
+	struct Metadata // sizeof(Metadata) == 152
+	{
+		using byte = char;
+		const static size_t NAME_LENGTH = 128;
+		char Name[NAME_LENGTH];
+		uint64_t Count;
+		uint64_t StorageSize;
+		DiskLoc FirstRecord;
+	};
+	struct Record // sizeof(Record) == 32
+	{
+		DiskLoc Next, Prev; // Cast to `Record`
+		uint64_t LengthWoHeader; //Wo == Without
+		// void *Data; // No need to declare a pointer
+	};
 	class DbfsManager
 	{
 	private:
 		// metadata's offset of fd/mmap array
 		const static int META_OFFSET = -1;
-		const static int NAME_LENGTH = 128;
-		const static int FILE_PER_DB = 32768;
+		const static size_t NAME_LENGTH = 128;
+		const static size_t FILE_PER_DB = 32768;
 		const static size_t BASE_SIZE = 16777216; // 16MiB
 		char DBName[NAME_LENGTH];
 		char RootPath[NAME_LENGTH];
@@ -27,6 +49,7 @@ namespace shiku
 		int *fd, _fd[FILE_PER_DB];
 		void **mem, *_mem[FILE_PER_DB];
 		int *DataFileCount;
+		friend void *GetAddrDlPoints(const DbfsManager &dbfsmgr, const DiskLoc &dl);
 	public:
 		using byte = char;
 		DbfsManager(void)
@@ -36,5 +59,10 @@ namespace shiku
 		DbfsManager(const char *_DBName, const char *_RootPath);
 		~DbfsManager(void);
 	};
+	inline void *GetAddrDlPoints(const DbfsManager &dbfsmgr, const DiskLoc &dl)
+	{
+		using byte = char;
+		return (byte*)dbfsmgr.mem[dl.file] + dl.offset;
+	}
 }
 #endif // SHIKU_DBFS_MANAGER_HPP_
--- shikud.cpp
+++ shikud.cpp
@@ -1,13 +1,16 @@
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <functional>
 #include "HttpServer.hpp"
+#include "DbfsManager.hpp"
 void Initialize(int argc, char *argv[])
 {
 	shiku::HttpServer *hs = new shiku::HttpServer(6207);
 }
 int main(int argc, char *argv[])
 {
+	// printf("%lu %lu %lu\n", sizeof(shiku::DiskLoc), sizeof(shiku::Metadata), sizeof(shiku::Record));
 	std::function<void(int, char*[])> Delegates[] = 
 	{
 		Initialize
